88feeab4e4c3be61165e279fb763f2a066b99f14
Imelda Hope <imeldalarusso@gmail.com>
2024-07-06 16:15:33 +0300

    creates and deletes DOM elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form');
+  
+  form.addEventListener('submit',(e) => {
+    e.preventDefault(); 
+    //use string ID instead   
+    createTaskList(e.target["new-task-description"].value); 
+    form.reset();       
+  })
 });
+
+function createTaskList(task){
+    let list = document.createElement('li');
+    let myButton = document.createElement('button');
+    myButton.addEventListener('click',deleteBtn);
+    myButton.textContent = "delete"
+    list.textContent = `${task} `
+    list.appendChild(myButton);
+    document.getElementById('tasks').appendChild(list);
+}
+
+function deleteBtn(e){
+  e.target.parentNode.remove();
+}
